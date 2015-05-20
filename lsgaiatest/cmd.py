f681b4cb750e47299173734f4a9a3e300ba92c1e
Geo Mealer <gmealer@mozilla.com>
2015-05-19 19:29:14 -0700

    Updated docstring

--- lsgaiatest/cmd.py
+++ lsgaiatest/cmd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""List what tests marionette would run for a given set of options."""
+"""List what tests gaiatest would run for a given set of options."""
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
